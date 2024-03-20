42d048cf42321ee3eabacd73afb732d3a45ff332
Agustin Musanti <138232526+AgustinMusanti@users.noreply.github.com>
2024-03-19 22:28:09 -0300

    script_gestión_riesgo_crediticio_agustin_musanti.sql

--- script_gestión_riesgo_crediticio_agustin_musanti.sql
+++ script_gestión_riesgo_crediticio_agustin_musanti.sql
@@ -4,14 +4,14 @@
 
     USE gestión_riesgo_crediticio;
 
-    CREATE TABLE  Clientes 
+    CREATE TABLE        Clientes 
     (
-     Cliente_ID   INT            NOT NULL AUTO_INCREMENT PRIMARY KEY
-    ,Nombre       VARCHAR(100)   NOT NULL
-    ,Apellido     VARCHAR(100)   NOT NULL
-    ,Telefono     VARCHAR(20)    NOT NULL  
-    ,Correo       VARCHAR(100)   UNIQUE 
-    ,Direccion    VARCHAR(255)   DEFAULT  'Sin información'
+     Cliente_ID         INT            NOT NULL AUTO_INCREMENT PRIMARY KEY
+    ,Nombre             VARCHAR(100)   NOT NULL
+    ,Apellido           VARCHAR(100)   NOT NULL
+    ,Telefono           VARCHAR(20)    NOT NULL  
+    ,Correo             VARCHAR(100)   UNIQUE 
+    ,Direccion          VARCHAR(255)   DEFAULT  'Sin información'
     )
 	COMMENT "Esta tabla contiene toda la información personal relacionada a los clientes"
     ;
@@ -19,7 +19,7 @@
     
     CREATE TABLE        Cuentas 
     (
-     Cuentas_ID         INT           NOT NULL AUTO_INCREMENT PRIMARY KEY
+     Cuentas_ID         INT            NOT NULL AUTO_INCREMENT PRIMARY KEY
     ,Cliente_ID         INT
     ,Numero             VARCHAR(20)
     ,Tipo               VARCHAR(50)
@@ -32,10 +32,10 @@
      
     CREATE TABLE        Transacciones 
     (
-     Transacciones_ID   INT           NOT NULL AUTO_INCREMENT PRIMARY KEY
-    ,Cuentas_ID          INT
-    ,Tipo               VARCHAR(50)   DEFAULT 'Desconocido' 
-    ,Monto              DECIMAL(10,2) DEFAULT 0.00 CHECK (Monto > 0)
+     Transacciones_ID   INT            NOT NULL AUTO_INCREMENT PRIMARY KEY
+    ,Cuentas_ID         INT
+    ,Tipo               VARCHAR(50)    DEFAULT 'Desconocido' 
+    ,Monto              DECIMAL(10,2)  DEFAULT 0.00 CHECK (Monto > 0)
     ,Fecha              DATE          
     )
 	COMMENT "En esta tabla se registran todas las transacciones que realiza el cliente con la institución financiera"
@@ -44,12 +44,12 @@
      
     CREATE TABLE        Prestamos 
     (
-     Prestamos_ID       INT           NOT NULL AUTO_INCREMENT PRIMARY KEY
+     Prestamos_ID       INT            NOT NULL AUTO_INCREMENT PRIMARY KEY
     ,Cliente_ID         INT
-    ,Monto              DECIMAL(10,2) DEFAULT 0.00 CHECK (Monto > 0)
+    ,Monto              DECIMAL(10,2)  DEFAULT 0.00 CHECK (Monto > 0)
     ,TasaInteres        DECIMAL(5,2)
-    ,Plazo              INT           DEFAULT 180 CHECK (Plazo >= 180)
-    ,Estado             VARCHAR(50)   DEFAULT 'Activo'
+    ,Plazo              INT            DEFAULT 180 CHECK (Plazo >= 180)
+    ,Estado             VARCHAR(50)    DEFAULT 'Activo'
     )
 	COMMENT "Esta tabla registra los préstamos que puedan llegar a realizar los clientes con la institución financiera"
     ;
@@ -57,9 +57,9 @@
      
     CREATE TABLE        Pagos_Prestamos 
     (
-     Pagos_Prest_ID     INT           NOT NULL AUTO_INCREMENT PRIMARY KEY
+     Pagos_Prest_ID     INT            NOT NULL AUTO_INCREMENT PRIMARY KEY
     ,Prestamos_ID        INT
-    ,Monto              DECIMAL(10,2) DEFAULT 0.00 CHECK (Monto > 0)
+    ,Monto              DECIMAL(10,2)  DEFAULT 0.00 CHECK (Monto > 0)
     ,Fecha              DATE          
     )
 	COMMENT "En esta tabla se registran los pagos de los préstamos por parte de los clientes"
@@ -68,9 +68,9 @@
      
     CREATE TABLE        Historial_Credito 
     (
-     Hist_Credito_ID    INT           NOT NULL AUTO_INCREMENT PRIMARY KEY
+     Hist_Credito_ID    INT            NOT NULL AUTO_INCREMENT PRIMARY KEY
     ,Cliente_ID         INT
-    ,Puntaje            INT           DEFAULT 0 CHECK (Puntaje >= 0)
+    ,Puntaje            INT            DEFAULT 0 CHECK (Puntaje >= 0)
     ,Observaciones      TEXT           
     )
 	COMMENT "Esta tabla registra el historial crediticio que posee cada cliente"
@@ -79,7 +79,7 @@
      
     CREATE TABLE        Modelos_Riesgo 
     (
-     Mod_Riesgo_ID      INT           NOT NULL AUTO_INCREMENT PRIMARY KEY
+     Mod_Riesgo_ID      INT            NOT NULL AUTO_INCREMENT PRIMARY KEY
     ,Nombre             VARCHAR(100)
     ,Descripcion        TEXT
     ,Coeficientes       TEXT
@@ -90,7 +90,7 @@
    
     CREATE TABLE        Eventos_Riesgo 
     (
-     Even_Riesgo_ID     INT           NOT NULL AUTO_INCREMENT PRIMARY KEY
+     Even_Riesgo_ID     INT            NOT NULL AUTO_INCREMENT PRIMARY KEY
     ,Descripcion        TEXT
     ,Fecha              DATE          
     )
@@ -98,17 +98,17 @@
     ;
    
    
-    ALTER TABLE Cuentas            ADD CONSTRAINT fk_cuen_clientes
-    FOREIGN KEY (Cliente_ID)       REFERENCES Clientes(Cliente_ID);
+    ALTER TABLE Cuentas                ADD CONSTRAINT fk_cuen_clientes
+    FOREIGN KEY (Cliente_ID)           REFERENCES Clientes(Cliente_ID);
      
-    ALTER TABLE Transacciones      ADD CONSTRAINT fk_trans_cuentas
-    FOREIGN KEY (Cuentas_ID)       REFERENCES Cuentas(Cuentas_ID);
+    ALTER TABLE Transacciones          ADD CONSTRAINT fk_trans_cuentas
+    FOREIGN KEY (Cuentas_ID)           REFERENCES Cuentas(Cuentas_ID);
    
-    ALTER TABLE Prestamos          ADD CONSTRAINT fk_pres_clientes
-    FOREIGN KEY (Cliente_ID)       REFERENCES Clientes(Cliente_ID);
+    ALTER TABLE Prestamos              ADD CONSTRAINT fk_pres_clientes
+    FOREIGN KEY (Cliente_ID)           REFERENCES Clientes(Cliente_ID);
      
-    ALTER TABLE Pagos_Prestamos    ADD CONSTRAINT fk_pago_prestamos
-    FOREIGN KEY (Prestamos_ID)     REFERENCES Prestamos(Prestamos_ID); 
+    ALTER TABLE Pagos_Prestamos        ADD CONSTRAINT fk_pago_prestamos
+    FOREIGN KEY (Prestamos_ID)         REFERENCES Prestamos(Prestamos_ID); 
    
-    ALTER TABLE Historial_Credito  ADD CONSTRAINT fk_hist_clientes
-    FOREIGN KEY (Cliente_ID)       REFERENCES Clientes(Cliente_ID);
+    ALTER TABLE Historial_Credito      ADD CONSTRAINT fk_hist_clientes
+    FOREIGN KEY (Cliente_ID)           REFERENCES Clientes(Cliente_ID);
