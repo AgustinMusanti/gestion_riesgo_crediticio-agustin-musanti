a60bbd029589dc6985e61c04df96358a9c54523e
Agustin Musanti <138232526+AgustinMusanti@users.noreply.github.com>
2024-04-25 19:45:43 -0300

    Estructura completa de la base de datos - Agustin Musanti.sql

--- Estructura completa de la base de datos - Agustin Musanti.sql
+++ Estructura completa de la base de datos - Agustin Musanti.sql
@@ -277,41 +277,43 @@
    -- Vistas
 
 
-    CREATE VIEW VistaClientesSaldoNegativo 
+    CREATE VIEW     VistaClientesSaldoNegativo 
     AS
-    SELECT      c.Cliente_ID, c.Nombre, c.Apellido, cu.Saldo
-    FROM        Clientes c
-    JOIN        Cuentas cu ON c.Cliente_ID = cu.Cliente_ID
-    WHERE       cu.Saldo < 0;
+    SELECT          c.Cliente_ID, c.Nombre, c.Apellido, cu.Saldo
+    FROM            Clientes c
+    JOIN            Cuentas cu ON c.Cliente_ID = cu.Cliente_ID
+    WHERE           cu.Saldo < 0;
 
 
-    CREATE VIEW VistaTransaccionesRecientes 
+    CREATE VIEW     VistaTransaccionesRecientes 
     AS
-    SELECT      Cuentas_ID, Tipo, Monto, Fecha
-    FROM        Transacciones
-    WHERE       Fecha >= DATE_SUB(CURDATE(), INTERVAL 30 DAY);
+    SELECT          Cuentas_ID, Tipo, Monto, Fecha
+    FROM            Transacciones
+    WHERE           Fecha >= DATE_SUB(CURDATE(), INTERVAL 30 DAY);
 
 
-    CREATE VIEW VistaTransaccionesMontosAltos 
+    CREATE VIEW     VistaTransaccionesMontosAltos 
     AS
-    SELECT      Cuentas_ID, Tipo, Monto, Fecha
-    FROM        Transacciones
-    WHERE       Monto > 2500;
+    SELECT          Cuentas_ID, Tipo, Monto, Fecha
+    FROM            Transacciones
+    WHERE           Monto > 2500;
 
 
-    CREATE VIEW VistaClientesHistorialCrediticioMalo 
+    CREATE VIEW     VistaClientesHistorialCrediticioMalo 
     AS
-    SELECT      c.Cliente_ID, c.Nombre, c.Apellido, hc.Puntaje
-    FROM        Clientes c
-    JOIN        Historial_Credito hc ON c.Cliente_ID = hc.Cliente_ID
-    WHERE       hc.Puntaje < 5;
+    SELECT          c.Cliente_ID, c.Nombre, c.Apellido, hc.Puntaje
+    FROM            Clientes c
+    JOIN            Historial_Credito hc ON c.Cliente_ID = hc.Cliente_ID
+    WHERE           hc.Puntaje < 5;
 
 
-    CREATE VIEW VistaDetallePrestamos 
+    CREATE VIEW     VistaDetallePrestamosActivos
     AS
-    SELECT      p.Prestamos_ID, c.Nombre AS Cliente, p.Monto, p.TasaInteres, p.Plazo, p.Estado
-    FROM        Prestamos p
-    JOIN        Clientes c ON p.Cliente_ID = c.Cliente_ID;
+    SELECT          p.Prestamos_ID, c.Nombre AS Cliente, p.Monto, p.TasaInteres, p.Plazo, p.Estado
+    FROM            Prestamos p
+    JOIN            Clientes c ON p.Cliente_ID = c.Cliente_ID
+    WHERE           p.Estado = 'Activo';
+
 
 
     --  Funciones
