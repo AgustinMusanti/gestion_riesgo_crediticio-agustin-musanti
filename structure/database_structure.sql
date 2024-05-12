d6397facef9d70bcb29d41bab7e7cce393c5c125
Agustin Musanti <138232526+AgustinMusanti@users.noreply.github.com>
2024-05-11 23:19:11 -0300

    >> structure

--- structure/database_structure.sql
+++ structure/database_structure.sql
@@ -6,12 +6,12 @@
 
     CREATE TABLE        Clientes 
     (
-     Cliente_ID         INT            NOT NULL AUTO_INCREMENT PRIMARY KEY
-    ,Nombre             VARCHAR(100)   NOT NULL
-    ,Apellido           VARCHAR(100)   NOT NULL
-    ,Telefono           VARCHAR(20)    NOT NULL  
-    ,Correo             VARCHAR(100)   UNIQUE 
-    ,Direccion          VARCHAR(255)   DEFAULT  'Sin informacion'
+     Cliente_ID         INT                NOT NULL AUTO_INCREMENT PRIMARY KEY
+    ,Nombre             VARCHAR(100)       NOT NULL
+    ,Apellido           VARCHAR(100)       NOT NULL
+    ,Telefono           VARCHAR(20)        NOT NULL  
+    ,Correo             VARCHAR(100)       UNIQUE 
+    ,Direccion          VARCHAR(255)       DEFAULT  'Sin informacion'
     )
 	COMMENT "Esta tabla contiene toda la informacion personal relacionada a los clientes"
     ;
@@ -19,12 +19,12 @@
     
     CREATE TABLE        Cuentas 
     (
-     Cuentas_ID         INT            NOT NULL AUTO_INCREMENT PRIMARY KEY
+     Cuentas_ID         INT                NOT NULL AUTO_INCREMENT PRIMARY KEY
     ,Cliente_ID         INT
     ,Numero             VARCHAR(20)
     ,Tipo               VARCHAR(50)
     ,Saldo              DECIMAL(10,2)
-    ,FechaApertura      DATE           DEFAULT (CURDATE())
+    ,FechaApertura      DATE               DEFAULT (CURDATE())
     )
 	COMMENT "Esta tabla registra datos relacionados a las cuentas que poseen los clientes"
     ;
@@ -32,11 +32,11 @@
      
     CREATE TABLE        Transacciones 
     (
-     Transacciones_ID   INT            NOT NULL AUTO_INCREMENT PRIMARY KEY
+     Transacciones_ID   INT                NOT NULL AUTO_INCREMENT PRIMARY KEY
     ,Cuentas_ID         INT
-    ,Tipo               VARCHAR(50)    DEFAULT 'Desconocido' 
+    ,Tipo               VARCHAR(50)        DEFAULT 'Desconocido' 
     ,Monto              DECIMAL(10,2)  
-    ,Fecha              DATE           DEFAULT (CURDATE())
+    ,Fecha              DATE               DEFAULT (CURDATE())
     )
 	COMMENT "En esta tabla se registran todas las transacciones que realiza el cliente con la institucion financiera"
     ;
@@ -44,12 +44,12 @@
      
     CREATE TABLE        Prestamos 
     (
-     Prestamos_ID       INT            NOT NULL AUTO_INCREMENT PRIMARY KEY
+     Prestamos_ID       INT                NOT NULL AUTO_INCREMENT PRIMARY KEY
     ,Cliente_ID         INT
     ,Monto              DECIMAL(10,2)  
     ,TasaInteres        DECIMAL(5,2)
-    ,Plazo              INT            DEFAULT 180  CHECK (Plazo >= 180)
-    ,Estado             VARCHAR(50)    DEFAULT 'Activo'
+    ,Plazo              INT                DEFAULT 180  CHECK (Plazo >= 180)
+    ,Estado             VARCHAR(50)        DEFAULT 'Activo'
     )
 	COMMENT "Esta tabla registra los prestamos que puedan llegar a realizar los clientes con la institucion financiera"
     ;
@@ -57,10 +57,10 @@
      
     CREATE TABLE        Pagos_Prestamos 
     (
-     Pagos_Prest_ID     INT            NOT NULL AUTO_INCREMENT PRIMARY KEY
+     Pagos_Prest_ID     INT                NOT NULL AUTO_INCREMENT PRIMARY KEY
     ,Prestamos_ID       INT
     ,Monto              DECIMAL(10,2)  
-    ,Fecha              DATE           DEFAULT (CURDATE())
+    ,Fecha              DATE               DEFAULT (CURDATE())
     )
 	COMMENT "En esta tabla se registran los pagos de los prestamos por parte de los clientes"
     ;
@@ -68,9 +68,9 @@
      
     CREATE TABLE        Historial_Credito 
     (
-     Hist_Credito_ID    INT            NOT NULL AUTO_INCREMENT PRIMARY KEY
+     Hist_Credito_ID    INT                NOT NULL AUTO_INCREMENT PRIMARY KEY
     ,Cliente_ID         INT
-    ,Puntaje            INT            DEFAULT 0 CHECK (Puntaje >= 0)
+    ,Puntaje            INT                DEFAULT 0 CHECK (Puntaje >= 0)
     ,Observaciones      TEXT           
     )
 	COMMENT "Esta tabla registra el historial crediticio que posee cada cliente"
@@ -79,7 +79,7 @@
      
     CREATE TABLE        Modelos_Riesgo 
     (
-     Mod_Riesgo_ID      INT            NOT NULL AUTO_INCREMENT PRIMARY KEY
+     Mod_Riesgo_ID      INT                NOT NULL AUTO_INCREMENT PRIMARY KEY
     ,Nombre             VARCHAR(100)
     ,Descripcion        TEXT
     ,Coeficientes       TEXT
@@ -90,36 +90,36 @@
    
     CREATE TABLE        Eventos_Riesgo 
     (
-     Even_Riesgo_ID     INT            NOT NULL AUTO_INCREMENT PRIMARY KEY
+     Even_Riesgo_ID     INT                NOT NULL AUTO_INCREMENT PRIMARY KEY
     ,Descripcion        TEXT
-    ,Fecha              DATE           DEFAULT (CURDATE())
+    ,Fecha              DATE               DEFAULT (CURDATE())
     )
 	COMMENT "Esta tabla es independiente de las demas, su finalidad es almacenar informacion sobre los eventos de riesgo"
     ;
    
    
-    ALTER TABLE   Cuentas                ADD CONSTRAINT fk_cuen_clientes
-    FOREIGN KEY   (Cliente_ID)           REFERENCES Clientes(Cliente_ID);
+    ALTER TABLE         Cuentas            ADD CONSTRAINT fk_cuen_clientes
+    FOREIGN KEY         (Cliente_ID)       REFERENCES Clientes(Cliente_ID);
 
      
-    ALTER TABLE   Transacciones          ADD CONSTRAINT fk_trans_cuentas
-    FOREIGN KEY   (Cuentas_ID)           REFERENCES Cuentas(Cuentas_ID)
-    ON DELETE                            CASCADE
-    ON UPDATE                            CASCADE;
+    ALTER TABLE         Transacciones      ADD CONSTRAINT fk_trans_cuentas
+    FOREIGN KEY         (Cuentas_ID)       REFERENCES Cuentas(Cuentas_ID)
+    ON DELETE                              CASCADE
+    ON UPDATE                              CASCADE;
 
 
-    ALTER TABLE   Prestamos              ADD CONSTRAINT fk_pres_clientes
-    FOREIGN KEY   (Cliente_ID)           REFERENCES Clientes(Cliente_ID)
-    ON DELETE                            CASCADE
-    ON UPDATE                            CASCADE;
+    ALTER TABLE         Prestamos          ADD CONSTRAINT fk_pres_clientes
+    FOREIGN KEY         (Cliente_ID)       REFERENCES Clientes(Cliente_ID)
+    ON DELETE                              CASCADE
+    ON UPDATE                              CASCADE;
 
 
-    ALTER TABLE   Pagos_Prestamos          ADD CONSTRAINT fk_pago_prestamos
-    FOREIGN KEY   (Prestamos_ID)         REFERENCES Prestamos(Prestamos_ID)
-    ON DELETE                            CASCADE
-    ON UPDATE                            CASCADE;
+    ALTER TABLE         Pagos_Prestamos    ADD CONSTRAINT fk_pago_prestamos
+    FOREIGN KEY         (Prestamos_ID)     REFERENCES Prestamos(Prestamos_ID)
+    ON DELETE                              CASCADE
+    ON UPDATE                              CASCADE;
 
 
-    ALTER TABLE   Historial_Credito      ADD CONSTRAINT fk_hist_clientes
-    FOREIGN KEY   (Cliente_ID)           REFERENCES Clientes(Cliente_ID);
+    ALTER TABLE         Historial_Credito  ADD CONSTRAINT fk_hist_clientes
+    FOREIGN KEY         (Cliente_ID)       REFERENCES Clientes(Cliente_ID);
 
